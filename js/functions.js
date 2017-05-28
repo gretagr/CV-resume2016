1bf9eb5e5e6187bcd9798081f46dd967d7f8a35f
Greta Grigelionyte <greta.grigelionyte@gmail.com>
2017-05-29 01:08:37 +0300

    1

--- js/functions.js
+++ js/functions.js
@@ -2,32 +2,22 @@ $(function() {
   $('progress').each(function() {
     var max = $(this).val();
     var time = 500;
-    $(this).val(0).delay(1500).animate({ value: max }, { duration: 1000, easeOutCirc: 'easeOutCirc' });
+    $(this).val(0).delay(500).animate({ value: max }, { duration: 1000, easeOutCirc: 'easeOutCirc' });
       });
 });
-
-var holder = document.getElementsByClassName("popup-image");
-var middle = document.getElementsByClassName("text");
 var cup = document.getElementById("cup");
 var comp = document.getElementById("comp");
 var ruler = document.getElementById("ruler");
 var note = document.getElementById("note");
 
-
-TweenMax.from(cup, 2, {width: 0, ease:Elastic.easeOut});
+TweenMax.to(cup, 2, {width: 80, ease:Elastic.easeOut});
 TweenMax.from(comp, 2, {width: 0, right: -100, ease:Back.easeOut});
-TweenMax.from(ruler, 2, {x: -850,y: -100, rotation:360, ease:Bounce.easeOut, delay: 1});
+TweenMax.from(ruler, 2, {x: -1000, y: -100, rotation:360, ease:Bounce.easeOut, delay: 1});
 TweenMax.from(note, 2, {x: -500, ease:Back.easeOut});
 
 window.addEventListener("resize", imgSize, false);
 
-function getPosition(element) {
-   var rect = element.getBoundingClientRect();
-   return {x:rect.left,y:rect.top};
-}
-var position = getPosition(comp);
-comp.style.right = (position.x - 1) + "px";
-
  function imgSize(){
-
+   "use strict";
+    //window.location.reload();
  };
